a4813bbd573f33238dcc6373faaf278079f30c8b
xpuentes93 <xpuentes93@gmail.com>
2020-08-23 00:14:18 -0500

    Added setup and draw functions

---
+++ Game of Life/sketch.js
@@ -0,0 +1,11 @@
+function setup(){
+    console.log(windowWidth, windowHeight);
+    
+    createCanvas(windowWidth, windowHeight - 200);
+
+    console.log(width, height);
+}
+
+function draw(){
+    background(0);
+}
