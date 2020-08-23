f23ebd012189fe30fdec32d28e3ac553e426577c
xpuentes93 <xpuentes93@gmail.com>
2020-08-23 00:53:33 -0500

    Diplying static grid on page

--- Game of Life/sketch.js
+++ Game of Life/sketch.js
@@ -1,13 +1,38 @@
-function setup(){
-    console.log(windowWidth, windowHeight);
-    
-    createCanvas(windowWidth, windowHeight - 200);
+let universe;
+let x;
+let y;
+let paused = false;
+let clear = false;
 
-    console.log(width, height);
+function setup(){    
+    createCanvas(windowWidth, windowHeight - 200);
+    x = width;
+    y = height;
+
+    universe = create2DArr(x, y);
+
+    for(let i = 0; i < x; i++){
+        for(let j = 0; j < y; j++){
+            universe[i][j] = 0;
+        }
+    }
 }
 
 function draw(){
-    background(0);
+    background(255);
+
+    for(let i = 0; i < x; i++){
+        for(let j = 0; j < y; j++){
+            let rectX = i * 20;
+            let rectY = j * 20;
+
+            if (universe[i][j] == 0) {
+                fill(255);
+                stroke(0);
+                rect(rectX, rectY, 20, 20);
+            }
+        }
+    }
 }
 
 function create2DArr(x, y){
